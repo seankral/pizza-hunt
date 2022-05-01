f895de4f385fbc5e6c01a934f3306e9a0854ecb8
seankral <seankral9@gmail.com>
2022-05-01 08:12:20 -0400

    created pizza model / established db connection using mongoose

---
+++ models/Pizza.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+
+  pizzaName: {
+    type: String
+  },
+  
+  createdBy: {
+    type: String
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+
+  size: {
+    type: String,
+    default: 'Large'
+  },
+
+  toppings: []
+
+});
+
+// create the pizza model using the pizzaschema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export pizza model
+module.exports = Pizza;
